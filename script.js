2d896e93c68725665cf2563287e153dd06ef3c9a
RichaLimbachiya <richalimbachiya2004@gmail.com>
2025-05-13 14:05:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-function toRoman(num) {
-    if (num <= 0) return ''; // Roman numerals have no representation for 0
+function convertToRoman(num) {
+  const romanMap = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
 
-    const symbols = [
-        ['M', 1000],
-        ['CM', 900],
-        ['D', 500],
-        ['CD', 400],
-        ['C', 100],
-        ['XC', 90],
-        ['L', 50],
-        ['XL', 40],
-        ['X', 10],
-        ['IX', 9],
-        ['V', 5],
-        ['IV', 4],
-        ['I', 1]
-    ];
+  let result = "";
 
-    let result = '';
-
-    for (let [roman, value] of symbols) {
-        while (num >= value) {
-            result += roman;
-            num -= value;
-        }
+  for (let [symbol, value] of romanMap) {
+    while (num >= value) {
+      result += symbol;
+      num -= value;
     }
+  }
 
-    return result;
+  return result;
 }
+
+console.log(convertToRoman(36)); // Output: XXXVI
+module.exports = convertToRoman;
